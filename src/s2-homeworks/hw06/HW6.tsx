58f7007af8c1637d32596ed4d69b92cba767ff3e
alekcandr1 <alekcandrmain@gmail.com>
2024-05-31 15:57:14 +0300

    HW6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,11 +19,11 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        setValue(restoreState<string>('hw6-editable-span-value', ''))
     }
 
     return (
-        <div id={'hw6'}>
+        <div id={'hw6'} className={s.hw6}>
             <div className={s2.hwTitle}>Homework #6</div>
 
             {/*демонстрация возможностей компоненты:*/}
