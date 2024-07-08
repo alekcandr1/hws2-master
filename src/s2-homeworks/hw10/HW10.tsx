de1e32e7224e9757daa92b16b54de88ff77e8bae
alekcandr1 <alekcandrmain@gmail.com>
2024-07-08 19:49:15 +0300

    HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppStoreType } from './bll/store'
+import { loadingAC } from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
-import {Loader} from './Loader'
+import { Loader } from './Loader'
 
 /*
 * 1 - в файле loadingReducer.ts дописать типы и логику
@@ -14,32 +14,40 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
+    const dispatch = useDispatch()
+    const state = useSelector<AppStoreType, {isLoading: boolean}>(state => state.loading)
+
+    console.log(state.isLoading)
+
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = state.isLoading
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
     }
 
     return (
-        <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
+        <div id={ 'hw10' }>
+            <div className={ s2.hwTitle }>Homework #10</div>
 
-            <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
+            <div className={ s2.hw }>
+                { isLoading ? (
+                    <div id={ 'hw10-loading' }>
+                        <Loader />
                     </div>
                 ) : (
                     <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
+                        id={ 'hw10-button-start-loading' }
+                        onClick={ setLoading }
                     >
                         Set loading...
                     </SuperButton>
-                )}
+                ) }
             </div>
         </div>
     )
